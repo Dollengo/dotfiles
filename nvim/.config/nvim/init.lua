c41c9ffbdfe0194a3657cea053e2993d78c4c78b
Dollengo <dollengo@outlook.com>
2024-11-24 13:33:16 -0300

    24day-11month-2024year

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-vim.cmd("colorscheme solarized-osaka-night")
+vim.cmd([[ colorscheme retrobox ]])
 vim.opt.relativenumber = false
 vim.opt.number = true
